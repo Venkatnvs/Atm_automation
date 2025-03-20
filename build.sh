b0b19648a5a24022e8dc0cbda4583d6a29d048e9
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2025-03-20 21:01:45 +0530

    Refactor build script to remove unnecessary comments and streamline package installation

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
-# sudo apt-get update
-# $ sudo apt-get install build-essential cmake
-# $ sudo apt-get install libopenblas-dev liblapack-dev
-# $ sudo apt-get install libx11-dev libgtk-3-dev
-
 set -o errexit
 
 # Install required packages
-sudo apt-get update
-sudo apt-get install -y build-essential cmake
-sudo apt-get install -y libopenblas-dev liblapack-dev
-sudo apt-get install -y libx11-dev libgtk-3-dev
-sudo apt-get install -y python3-opencv
+apt-get update
+apt-get install -y build-essential cmake
+apt-get install -y libopenblas-dev liblapack-dev
+apt-get install -y libx11-dev libgtk-3-dev
+apt-get install -y python3-opencv
 
 # Install dependencies
 pip install -r requirements.txt
