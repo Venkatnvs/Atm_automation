0acb2cd722dd4e5d6dc1af0053f1ad8c0731e95e
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2025-03-20 21:03:39 +0530

    Remove redundant package installation commands from build script

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 set -o errexit
 
-# Install required packages
-apt-get update
-apt-get install -y build-essential cmake
-apt-get install -y libopenblas-dev liblapack-dev
-apt-get install -y libx11-dev libgtk-3-dev
-apt-get install -y python3-opencv
-
 # Install dependencies
 pip install -r requirements.txt
